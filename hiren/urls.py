dca3e50c318463408c339326965e8723fa82253f
Ashutosh Das <git.pyprism@gmail.com>
2018-01-13 21:17:40 +0600

    added new route

--- hiren/urls.py
+++ hiren/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from django.conf import settings
+from django.contrib.auth.views import logout
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('logout/', logout, {'next_page': '/'}, name='logout'),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        re_path(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
+
