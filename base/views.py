530272c5a14bc064b2bcf0ecb08615254f382172
Ashutosh Das <git.pyprism@gmail.com>
2018-01-14 16:02:16 +0600

    Added login

--- base/views.py
+++ base/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.hashers import make_password
+from django.contrib import auth
+from django.contrib import messages
 
-# Create your views here.
+
+def login(request):
+    """
+    Handles authentication
+    :param request:
+    :return:
+    """
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+        if user:
+            auth.login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Username/Password is not valid!')
+            return redirect('login')
+    else:
+        return render(request, 'base/login.html')
+
