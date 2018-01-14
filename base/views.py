7b6d01b6e0411354e3d3ece898b2ee97a6471176
Ashutosh Das <git.pyprism@gmail.com>
2018-01-14 18:27:12 +0600

    added signup

--- base/views.py
+++ base/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.hashers import make_password
 from django.contrib import auth
 from django.contrib import messages
+from .models import Account
+from django.db.utils import IntegrityError
 
 
 def login(request):
@@ -12,17 +14,41 @@ def login(request):
     :return:
     """
     if request.user.is_authenticated:
-        return redirect('dashboard')
+        return redirect('inbox')
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = auth.authenticate(username=username, password=password)
         if user:
             auth.login(request, user)
-            return redirect('dashboard')
+            return redirect('inbox')
         else:
             messages.error(request, 'Username/Password is not valid!')
             return redirect('login')
     else:
         return render(request, 'base/login.html')
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('inbox')
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        count = Account.objects.count()
+        acc = Account(username=username, password=make_password(password))
+        if count == 0:
+            acc.is_admin = True
+        else:
+            pass
+        try:
+            acc.save()
+        except IntegrityError:
+            messages.error(request, "username is not available!")
+            return redirect('signup')
+        messages.success(request, 'Account created successfully!')
+        return redirect('signup')
+    else:
+        return render(request, 'base/signup.html')
+
 
