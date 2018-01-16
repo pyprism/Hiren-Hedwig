9f44465198afb7513b193211b1d8725635a3e44d
Ashutosh Das <git.pyprism@gmail.com>
2018-01-16 16:40:56 +0600

    added new route

--- base/urls.py
+++ base/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('settings/', views.settings, name='settings'),
+    path('domain/<int:pk>/delete/', views.delete_domain, name='delete_domain'),
     path('domain/<int:pk>/', views.update_domain, name='update_domain'),
     path('domain/', views.create_domain, name='create_domain'),
     path('logout/', logout, {'next_page': '/'}, name='logout'),
