7fb61453c6503b5c191b6085ef806a25b8b6e7e0
Ashutosh Das <git.pyprism@gmail.com>
2018-01-16 21:26:59 +0600

    wip

--- utils/mailgun.py
+++ utils/mailgun.py
@@ -1,8 +1,14 @@
 import requests
 import django
 import os
+# import asyncio
+# loop = asyncio.get_event_loop()
+# loop.run_in_executor(None, 'task')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hiren.settings")
 django.setup()
 
 from base.models import MailGun
+
+def send_mail():
+    pass
