39e8ed588d700cd94c268cd81ef87e7aa3c2c8d9
Ashutosh Das <git.pyprism@gmail.com>
2018-01-18 02:34:13 +0600

    wip attachment

--- mail/views.py
+++ mail/views.py
@@ -4,6 +4,7 @@ from .forms import MailForm
 from base.models import MailGun
 from django.shortcuts import get_object_or_404
 from django.contrib import messages
+from .models import Attachment
 
 
 @login_required
@@ -13,29 +14,44 @@ def inbox(request):
 
 @login_required
 def compose(request):
+    """
+    Add new email to system
+    :param request:
+    :return:
+    """
     if request.method == 'POST':
         files = request.FILES.getlist('attachment')
-        if len(files) == 0:
-            compose = MailForm(request.POST)
-            if compose.is_valid():
-                domain_str = request.POST.get('mail_from')
-                domain = domain_str.split('@')[1]
-                mailgun = get_object_or_404(MailGun, name=domain, user=request.user)
-                compose_obj = compose.save(commit=False)
-                compose_obj.domain = mailgun
-                compose_obj.user = request.user
-                if request.POST.get('send'):  # detect which submit was clicked
-                    compose_obj.state = 'Q'
-                    compose_obj.save()
-                    messages.success(request, 'Mail has been sent.')
-                if request.POST.get('draft'):
-                    compose_obj.state = 'D'
-                    compose_obj.save()
-                    messages.success(request, 'Mail saved as draft.')
-            else:
-                messages.warning(request, compose.errors)
+        compose = MailForm(request.POST)
+        if compose.is_valid():
+            domain_str = request.POST.get('mail_from')
+            domain = domain_str.split('@')[1]  # extract domain name for mailgun api
+            mailgun = get_object_or_404(MailGun, name=domain, user=request.user)
+            compose_obj = compose.save(commit=False)
+            compose_obj.domain = mailgun
+            compose_obj.user = request.user
+            if len(files) > 0:
+                compose_obj.attachment = True
+            if request.POST.get('send'):  # detect which submit was clicked
+                compose_obj.state = 'Q'
+                hiren = compose_obj.save()
+                messages.success(request, 'Mail has been sent.')
+            if request.POST.get('draft'):
+                compose_obj.state = 'D'
+                hiren = compose_obj.save()
+                messages.success(request, 'Mail saved as draft.')
+            if len(files) > 0:
+                for file in files:
+                    bunny = Attachment(
+                        user=request.user,
+                        mail=hiren.id,
+                        file_name=file.name,
+                        attachment=file
+                    )
+                    bunny.save()
         else:
-            messages.warning(request, 'attachment!!')
+            messages.warning(request, compose.errors)
+        # else:
+        #     messages.warning(request, 'attachment!!')
         redirect('compose')
     return render(request, 'mail/compose.html')
 
