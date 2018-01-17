65a8a0783a253a1bc71d7b3b0fb9bb7b19394800
Ashutosh Das <git.pyprism@gmail.com>
2018-01-17 12:40:40 +0600

    wip

--- mail/views.py
+++ mail/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def inbox(request):
     return render(request, 'mail/inbox.html')
+
+
+@login_required
+def compose(request):
+    return render(request, 'mail/compose.html')
+
+
+
