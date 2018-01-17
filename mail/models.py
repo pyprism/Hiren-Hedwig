cb8776546f598298b2b33b256988c8a797dbddab
Ashutosh Das <git.pyprism@gmail.com>
2018-01-17 17:20:40 +0600

    added new models

--- mail/models.py
+++ mail/models.py
@@ -1,5 +1,9 @@
+import random
+import string
+import os
 from django.db import models
-from base.models import Account
+from base.models import Account, MailGun
+from django.utils import timezone
 
 
 class Contact(models.Model):
@@ -15,6 +19,51 @@ class Contact(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
+class Mail(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    mail_from = models.CharField(max_length=500)
+    mail_to = models.CharField(max_length=2000)
+    cc = models.CharField(max_length=2000, blank=True, null=True)
+    bcc = models.CharField(max_length=2000, blank=True, null=True)
+    subject = models.CharField(max_length=1000, blank=True, null=True)
+    body = models.TextField()
+    mail_state = (
+        ('Q', 'Queue'),
+        ('S', 'Success'),
+        ('D', 'Draft'),
+        ('R', 'Received'),
+        ('T', 'Trash'),
+    )
+    state = models.CharField(choices=mail_state, max_length=1)
+    emotional_attachment = models.BooleanField(default=False)   # lol :P emotional_attachment == mail attachment  :D
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+
+def create_random_string(length=30):
+    if length <= 0:
+        length = 30
+
+    symbols = string.ascii_lowercase + string.ascii_uppercase + string.digits
+    return ''.join([random.choice(symbols) for x in range(length)])
+
+
+def upload_to(instance, filename):  # for avoiding file name attacks
+    now = timezone.now()
+    filename_base, filename_ext = os.path.splitext(filename)
+    return 'hiren/attachment/{}_{}{}'.format(
+        now.strftime("%Y/%m/%d/%Y%m%d%H%M%S"),
+        create_random_string(),
+        filename_ext.lower()
+    )
+
+
+class Attachment(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    mail = models.ForeignKey(Mail, on_delete=models.CASCADE)
+    file_name = models.CharField(max_length=100)
+    attachment = models.FileField(upload_to=upload_to)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
