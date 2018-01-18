ef0831fc1d489774a94e12efb1f0b96bcb7dcb9b
Ashutosh Das <git.pyprism@gmail.com>
2018-01-18 13:38:52 +0600

    wip

--- utils/mailgun.py
+++ utils/mailgun.py
@@ -8,7 +8,34 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hiren.settings")
 django.setup()
 
-from base.models import MailGun
+from base.models import MailGun, Cron
+from mail.models import Mail
 
-def send_mail():
-    pass
+
+def send_mail(domain, api):
+    cron, created = Cron.objects.get_or_create(task='S', lock=False)
+    if not cron.lock:
+        cron.lock = True
+        cron.save()
+        mails = Mail.objects.filter(state='Q').select_related('domain')
+        for mail in mails:
+            if not mail.emotional_attachment:
+                hiren = requests.post(
+                    "https://api.mailgun.net/v3/" + mail.domain.name + "/messages",
+                    auth=("api", mail.domain.key),
+                    data={"from": mail.mail_from,
+                          "to": [mail.mail_to],
+                          "cc": [mail.cc],
+                          "bcc": [mail.bcc],
+                          "subject": mail.subject,
+                          "html": mail.body})
+                bunny = hiren.json()
+                mail.message_id = bunny['id']
+                mail.state = 'S'
+                mail.save()
+            else:
+                pass
+
+
+send_mail()
+
