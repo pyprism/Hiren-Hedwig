99f1840867712a88f89bf8f81e913ee6dd53407b
Ashutosh Das <git.pyprism@gmail.com>
2018-01-23 11:31:28 +0600

    changed url

--- mail/urls.py
+++ mail/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from mail import views
 
 urlpatterns = [
-    path('', views.inbox, name='inbox'),
+    path('inbox/', views.inbox, name='inbox'),
     path('compose/', views.compose, name='compose'),
     path('sent/', views.sent, name='sent'),
     path('draft/', views.draft, name='draft'),
