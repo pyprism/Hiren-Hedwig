5b576fce71c1e0568ef1a5cfb991483def5a69eb
Ashutosh Das <git.pyprism@gmail.com>
2018-01-24 09:49:20 +0600

    exclude some fields

--- mail/forms.py
+++ mail/forms.py
@@ -5,5 +5,5 @@ from .models import Mail
 class MailForm(ModelForm):
     class Meta:
         model = Mail
-        exclude = ('user', 'state', 'domain', 'message_id')
+        exclude = ('user', 'state', 'domain', 'message_id', 'sane_body', 'received_datetime')
 
