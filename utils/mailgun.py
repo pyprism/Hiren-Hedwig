18b3e94b2f1ce95a572fc96c326ff3dd9bd7428d
Ashutosh Das <git.pyprism@gmail.com>
2018-01-26 20:18:29 +0600

    another fix

--- utils/mailgun.py
+++ utils/mailgun.py
@@ -83,6 +83,17 @@ def send_mail():
         cron.save()
 
 
+BLEACH_VALID_TAGS = ['p', 'b', 'i', 'strike', 'ul', 'li', 'ol', 'br',
+                     'span', 'blockquote', 'hr', 'a', 'img']
+BLEACH_VALID_ATTRS = {
+    'span': ['style', ],
+    'p': ['align', ],
+    'a': ['href', 'rel'],
+    'img': ['src', 'alt', 'style'],
+}
+BLEACH_VALID_STYLES = ['color', 'cursor', 'float', 'margin']
+
+
 def items_process(items, mail):
     """
     Process and save incoming mail
@@ -98,7 +109,7 @@ def items_process(items, mail):
                 Mail.objects.create(domain=mail, user=mail.user, mail_from=bunny['From'],
                                     mail_to=bunny['To'], subject=bunny['subject'],
                                     message_id=item['message']['headers']['message-id'], body=bunny['body-html'],
-                                    sane_body=bleach.clean(bunny['body-html'], strip=False, strip_comments=True),
+                                    sane_body=bleach.clean(bunny['body-html'], BLEACH_VALID_TAGS, BLEACH_VALID_ATTRS, BLEACH_VALID_STYLES),
                                     state='R', received_datetime=to_datetime(bunny['Date']))
             else:
                 logger.error('item processor failed', exc_info=True, extra={
