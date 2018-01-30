d118b85df3a1234317b51db043292e14092bb8a5
Ashutosh Das <git.pyprism@gmail.com>
2018-01-30 22:38:45 +0600

    added new test

--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.urls import resolve, reverse
+from django.test import TestCase, TransactionTestCase
+from django.contrib.auth.models import User
+from django.test import Client
+from . import views
+from .models import Account
+from freezegun import freeze_time
+from django.utils import timezone
 
-# Create your tests here.
+
+class LoginViewTest(TestCase):
+
+    def setUp(self):
+        Account.objects.create(username='hiren', password="xyz")
+
+    def test_login_url_resolves_to_login_view(self):
+        found = resolve(reverse('login'))
+        self.assertEqual(found.func, views.login)
+
+
+
+
+
+
