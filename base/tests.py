0e5075ff6d287b5dca0fe9aa9c1ec38bf0126548
Ashutosh Das <git.pyprism@gmail.com>
2018-01-30 23:40:41 +0600

    more test

--- base/tests.py
+++ base/tests.py
@@ -11,11 +11,20 @@ from django.utils import timezone
 class LoginViewTest(TestCase):
 
     def setUp(self):
-        Account.objects.create(username='hiren', password="xyz")
+        Account.objects.create_user(username='hiren', password="xyz")
+        self.c = Client()
 
     def test_login_url_resolves_to_login_view(self):
         found = resolve(reverse('login'))
         self.assertEqual(found.func, views.login)
+
+    def test_auth(self):
+        respond = self.c.post(reverse('login'), {'username': 'hiren', 'password': 'xyz'})
+        self.assertRedirects(respond, reverse('inbox'))
+
+    def test_redirect_for_unauthenticated_user_works(self):
+        response = self.c.get(reverse('inbox'))
+        self.assertRedirects(response, '/?next=' + reverse('inbox'))
 
 
 
