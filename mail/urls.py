0a74b4cb8bd6f339549f571e3bb19eaa1f5e2173
Ashutosh Das <git.pyprism@gmail.com>
2018-02-12 16:27:17 +0600

    Added new route

--- mail/urls.py
+++ mail/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('inbox/', views.inbox, name='inbox'),
     path('thread/<int:pk>/', views.inbox_details, name='thread'),
     path('thread/delete/<int:thread_id>/<int:mail_id>/', views.thread_delete, name='thread_delete'),
+    path('thread/reply/<int:thread_id>/<int:mail_id>/', views.thread_reply, name='thread_reply'),
     path('compose/', views.compose, name='compose'),
     path('sent/', views.sent, name='sent'),
     path('draft/', views.draft, name='draft'),
