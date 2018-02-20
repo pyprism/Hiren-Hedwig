9858493421110d133b099810c115b57eb7c78875
Ashutosh Das <git.pyprism@gmail.com>
2018-02-20 13:01:16 +0600

    wip

---
+++ static/js/components/inbox.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Inbox extends Component {
+
+  render() {
+    return (
+     <div>
+         Hellos
+     </div>
+    );
+  }
+}
+
+ReactDOM.render(<Inbox />, document.getElementById('inbox'));
