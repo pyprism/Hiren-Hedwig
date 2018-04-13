aa3a06f62e10f5464fec060a4fef3d826e147fce
Ashutosh Das <git.pyprism@gmail.com>
2018-04-13 16:17:21 +0600

    changed script

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,7 @@
 const path = require("path");
 module.exports = {
     entry: {
-        "notebook": "./static/components/notebooks.js",
-        "notebook_create": "./static/components/notebook_create.js",
-        "notebook_by_id": "./static/components/notebook_by_id.js",
-        "note_create": "./static/components/note_create.js",
-        "note_by_id": "./static/components/note_by_id.js",
-        "note_edit": "./static/components/note_edit.js",
+        "GenerateKey": "./bunny/GenerateKey.js"
     },
     output: {
         path: path.resolve(__dirname, "static"),
