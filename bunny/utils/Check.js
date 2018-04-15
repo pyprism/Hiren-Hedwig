fc9841716e788a6547882940d8474d43322f2ca0
Ashutosh Das <git.pyprism@gmail.com>
2018-04-15 12:15:06 +0600

    fixed typo

--- bunny/utils/Check.js
+++ bunny/utils/Check.js
@@ -2,7 +2,7 @@ import swal from "sweetalert2";
 
 window.onload = function () {
     if(!sessionStorage["private_key"] || !sessionStorage["public_key"]) {
-        swal("Opps..", "Keys are empty, Renter?", "error").then(() => {
+        swal("Opps..", "Keys are empty, Reenter?", "error").then(() => {
             window.location.replace("/unlock/");
         });
     }
