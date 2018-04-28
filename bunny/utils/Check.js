5f7defc8384b9971de1d0c1e3766f0af5955a62d
Ashutosh Das <git.pyprism@gmail.com>
2018-04-28 12:46:12 +0600

    fix a bug

--- bunny/utils/Check.js
+++ bunny/utils/Check.js
@@ -1,9 +1,7 @@
 import swal from "sweetalert2";
 
-window.onload = function () {
-    if(!sessionStorage["private_key"] || !sessionStorage["public_key"]) {
-        swal("Opps..", "Keys are empty, Re-enter?", "error").then(() => {
-            window.location.replace("/unlock/");
-        });
-    }
-};
+if(!sessionStorage["private_key"] || !sessionStorage["public_key"]) {
+    swal("Opps..", "Keys are empty, Re-enter?", "error").then(() => {
+        window.location.replace("/unlock/");
+    });
+}
