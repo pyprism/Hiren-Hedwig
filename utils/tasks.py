295da29657684e8db80639604b94ecfe01891914
Ashutosh Das <git.pyprism@gmail.com>
2018-05-05 09:37:25 +0600

    added key import task

--- utils/tasks.py
+++ utils/tasks.py
@@ -1,6 +1,7 @@
 import requests
 import django
 import os
+import gnupg
 import logging
 from datetime import datetime, timedelta
 from email.utils import parsedate_tz
@@ -9,7 +10,7 @@ import tempfile
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hiren.settings")
 django.setup()
 
-from base.models import MailGun, Cron, Pgpkey
+from base.models import MailGun, Cron, Pgpkey, Account
 from mail.models import Mail, Attachment, Thread
 from django.core import files
 from django.core.exceptions import ObjectDoesNotExist
@@ -18,6 +19,12 @@ from celery import shared_task
 
 @shared_task
 def gen_fingerprint(username, pubkey):
-    print("called.........................")
+    gpg = gnupg.GPG(binary='/usr/bin/gpg', homedir='./keys')
+    result = gpg.import_keys(pubkey)
+    account = Account.objects.get(username=username)
+    pgp = Pgpkey.objects.get(user=account)
+    pgp.finger_print = result.fingerprints[0]
+    pgp.save()
+    print("Done")
 
 
