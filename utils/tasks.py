6699b986d867fb15ea13e6dc56cd1bd7107e180a
Ashutosh Das <git.pyprism@gmail.com>
2018-05-06 10:19:40 +0600

    fixed transaction

--- utils/tasks.py
+++ utils/tasks.py
@@ -16,6 +16,7 @@ from django.core import files
 from django.core.exceptions import ObjectDoesNotExist
 from celery import shared_task
 from .bunny import encrypt_mail
+from django.db import transaction
 
 
 @shared_task
@@ -39,45 +40,46 @@ logger = logging.getLogger(__name__)
 
 @shared_task
 def send_mail():
-    mails = Mail.objects.filter(state='Q').select_related('domain').select_for_update()
-    for mail in mails:
-        if not mail.emotional_attachment:
-            hiren = requests.post(
-                "https://api.mailgun.net/v3/" + mail.domain.name + "/messages",
-                auth=("api", mail.domain.key),
-                data={"from": mail.mail_from,
-                      "to": [mail.mail_to],
-                      "cc": [mail.cc],
-                      "bcc": [mail.bcc],
-                      "subject": mail.subject,
-                      "h:In-Reply-To": mail.in_reply_to,
-                      "html": mail.body})
-        else:
-            attachments = []
-            for attachment in Attachment.objects.filter(mail=mail):
-                attachments.append((("attachment", (attachment.file_name,
-                                                    open(attachment.file_obj.path, "rb").read()))))
-            hiren = requests.post(
-                "https://api.mailgun.net/v3/" + mail.domain.name + "/messages",
-                auth=("api", mail.domain.key),
-                files=attachments,
-                data={"from": mail.mail_from,
-                      "to": [mail.mail_to],
-                      "cc": [mail.cc],
-                      "bcc": [mail.bcc],
-                      "subject": mail.subject,
-                      "html": mail.body})
-        if hiren.status_code == 200:  # catch and report failed mail
-            bunny = hiren.json()
-            mail.message_id = bunny['id']
-            mail.state = 'S'
-            mail.save()
-            encrypt_mail(mail.pk)
-        else:
-            mail.state = 'F'
-            mail.save()
-            logger.error('Email sending failed!', exc_info=True, extra={
-                'request': hiren.json(),
-            })
+    with transaction.atomic():
+        mails = Mail.objects.filter(state='Q').select_related('domain').select_for_update().exclude(domain=None)
+        for mail in mails:
+            if not mail.emotional_attachment:
+                hiren = requests.post(
+                    "https://api.mailgun.net/v3/" + mail.domain.name + "/messages",
+                    auth=("api", mail.domain.key),
+                    data={"from": mail.mail_from,
+                          "to": [mail.mail_to],
+                          "cc": [mail.cc],
+                          "bcc": [mail.bcc],
+                          "subject": mail.subject,
+                          "h:In-Reply-To": mail.in_reply_to,
+                          "html": mail.body})
+            else:
+                attachments = []
+                for attachment in Attachment.objects.filter(mail=mail):
+                    attachments.append((("attachment", (attachment.file_name,
+                                                        open(attachment.file_obj.path, "rb").read()))))
+                    hiren = requests.post(
+                        "https://api.mailgun.net/v3/" + mail.domain.name + "/messages",
+                        auth=("api", mail.domain.key),
+                        files=attachments,
+                        data={"from": mail.mail_from,
+                              "to": [mail.mail_to],
+                              "cc": [mail.cc],
+                              "bcc": [mail.bcc],
+                              "subject": mail.subject,
+                              "html": mail.body})
+            if hiren.status_code == 200:  # catch and report failed mail
+                bunny = hiren.json()
+                mail.message_id = bunny['id']
+                mail.state = 'S'
+                mail.save()
+                encrypt_mail(mail.pk)
+            else:
+                mail.state = 'F'
+                mail.save()
+                logger.error('Email sending failed!', exc_info=True, extra={
+                    'request': hiren.json(),
+                })
 
 
