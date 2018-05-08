213bfc5fbbbe211aa7f99f5d821be7d0b28ddf2f
Ashutosh Das <git.pyprism@gmail.com>
2018-05-08 11:05:31 +0600

    wip

--- bunny/components/Sent.js
+++ bunny/components/Sent.js
@@ -2,11 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 import swal from "sweetalert2";
 import "regenerator-runtime/runtime";
+import BootstrapTable from 'react-bootstrap-table-next';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
 
 class Sent extends React.Component {
-    constructor() {
-        super();
+    constructor(prop) {
+        super(prop);
+        this.products = [
+            {"id": 1, "name": "Xxx", "price": "521$", "date": ""},
+            {"id": 2, "name": "444", "price": "421$", "date": "sss"},
+        ];
+        this.columns = [{
+            dataField: 'id',
+            text: 'From'
+        }, {
+            dataField: 'name',
+            text: 'To'
+        }, {
+            dataField: 'price',
+            text: 'Subject'
+        },
+            {
+                dataField: "date",
+                text: "Date"
+            }];
         this.state = {
             from: "",
             to: "",
@@ -16,6 +36,16 @@ class Sent extends React.Component {
             body: "",
             attachment: ""
         }
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return(
+            <BootstrapTable keyField='id' data={ this.products } columns={ this.columns } />
+        )
     }
 
 }
