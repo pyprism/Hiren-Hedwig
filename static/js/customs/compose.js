0c02883fae27e323884aa6196fad4112685200e5
Ashutosh Das <git.pyprism@gmail.com>
2018-05-15 13:26:14 +0600

    Added new script

---
+++ static/js/customs/compose.js
@@ -0,0 +1,47 @@
+/**
+ * Form body editor
+ */
+let quill;
+window.onload = function () {
+    quill = new Quill('#mailBody', {
+        modules: {
+            toolbar: [
+                [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
+                [{size: []}],
+                ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+                [{'list': 'ordered'}, {'list': 'bullet'},
+                    {'indent': '-1'}, {'indent': '+1'}],
+                ['link', 'image', 'video'],
+            ],
+            clipboard: {
+                // toggle to add extra line breaks when pasting HTML:
+                matchVisual: false,
+            }
+        },
+        theme: 'snow'
+    });
+};
+
+let form = document.querySelector('form');
+form.onsubmit = function() {
+    // Populate hidden form on submit
+    let body = document.querySelector('input[name=body]');
+    body.value = quill.root.innerHTML;
+    return true;
+};
+
+/**
+ * Contact list generator
+ */
+(function contacts() {
+    $.ajax({
+        type: "GET",
+        url: "/contact_ajax/",
+        success: function (data) {
+            console.log(data);
+        },
+        error: function (err) {
+            console.error(err);
+        }
+    })
+})();
