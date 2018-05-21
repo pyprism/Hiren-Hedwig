6bb1a4b32927b6747febfa6b41281b03831b7a5e
Ashutosh Das <git.pyprism@gmail.com>
2018-05-21 11:10:57 +0600

    added paginator

---
+++ utils/paginator.py
@@ -0,0 +1,43 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core import serializers
+
+
+def terminator(request, obj, item=25):
+    """
+    Hasta la vista, baby ....  reusable paginator
+    :param request:
+    :param obj: db object
+    :param item: optional number of item per page
+    :return:
+    """
+    paginator = Paginator(obj, item)
+    page = request.GET.get('page')
+    try:
+        bunny = paginator.page(page)
+    except PageNotAnInteger:
+        # If bunny is not an integer, deliver first page.
+        bunny = paginator.page(1)
+    except EmptyPage:
+        bunny = paginator.page(paginator.num_pages)
+    bugs = []
+    for i in bunny.object_list:
+        hiren = {}
+        hiren['id'] = i.id
+        hiren['mail_from'] = i.mail_from
+        hiren['mail_to'] = i.mail_to
+        hiren['cc'] = i.cc
+        hiren['bcc'] = i.bcc
+        hiren['subject'] = i.subject
+        hiren['body'] = i.body
+        hiren['state'] = i.state
+        hiren['emotional_attachment'] = i.emotional_attachment
+        hiren['received_datetime'] = i.received_datetime
+        hiren['created_at'] = i.created_at.strftime("%d %m %Y, %I:%M:%S %p")
+        bugs.append(hiren)
+    hiren = {"obj": bugs, "totalSize": paginator.count, "sizePerPage": 25,
+             "pageStartIndex": bunny.start_index(), "page": bunny.number}
+    return hiren
+
+
+
+
