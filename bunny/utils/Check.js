a57791d280d9dd38876a146baf9b019a26a9086d
Ashutosh Das <git.pyprism@gmail.com>
2018-05-22 14:36:14 +0600

    added new field check

--- bunny/utils/Check.js
+++ bunny/utils/Check.js
@@ -1,7 +1,9 @@
 import swal from "sweetalert2";
 
-if(!sessionStorage["private_key"] || !sessionStorage["public_key"]) {
-    swal("Opps..", "Keys are empty, Re-enter?", "error").then(() => {
-        window.location.replace("/unlock/");
-    });
+if (!sessionStorage["private_key"] || !sessionStorage["public_key"]) {
+    if (!sessionStorage["passphrase"]) {
+        swal("Opps..", "Keys are empty, Re-enter?", "error").then(() => {
+            window.location.replace("/unlock/");
+        });
+    }
 }
