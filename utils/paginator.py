896855dbb2eeadc46bfdf94f76f9b8207feb90c1
Ashutosh Das <git.pyprism@gmail.com>
2018-05-26 13:11:50 +0600

    changed pagination number :smile:

--- utils/paginator.py
+++ utils/paginator.py
@@ -2,7 +2,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core import serializers
 
 
-def terminator(request, obj, item=22, ajax=False):
+def terminator(request, obj, item=18, ajax=False):
     """
     Hasta la vista, baby ....  reusable paginator
     :param request:
