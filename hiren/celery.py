0b1a039c9d529e02a9e5b8500f3527fcee40f4ec
Ashutosh Das <git.pyprism@gmail.com>
2018-07-11 18:11:02 +0600

    wip

--- hiren/celery.py
+++ hiren/celery.py
@@ -1,11 +1,18 @@
 import os
 from celery import Celery
+# from utils.tasks import incoming_mail
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hiren.settings')
 
 app = Celery('hiren')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+
+# @app.on_after_configure.connect
+# def setup_periodic_tasks(sender, **kwargs):
+#     sender.add_periodic_task(5.0, 'utils.tasks.incoming_mail()', name='run every 5 minute')
+
 
 app.autodiscover_tasks()
 
