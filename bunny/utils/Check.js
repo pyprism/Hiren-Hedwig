aab42e4fd902cc77d3eb58ed408d70f913505f42
Ashutosh Das <git.pyprism@gmail.com>
2019-10-24 18:13:31 +0600

    nuked for good

--- bunny/utils/Check.js
+++
@@ -1,9 +0,0 @@
-import swal from "sweetalert2";
-
-if (!sessionStorage["private_key"] || !sessionStorage["public_key"]) {
-    if (!sessionStorage["passphrase"]) {
-        swal("Opps..", "Keys are empty, Re-enter?", "error").then(() => {
-            window.location.replace("/unlock/");
-        });
-    }
-}
